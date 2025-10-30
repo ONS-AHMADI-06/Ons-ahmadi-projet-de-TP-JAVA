c747538b097a348d47c9926b88330b893c2049c6
ONS-AHMADI-06 <ahmadions@fstsbz.u-kairouan.tn>
2025-10-30 09:36:23 +0100

    Refactor exercice2Tp1 to streamline input and output
    Removed unnecessary prompts for string input and simplified the code.

--- repository/onsTp1/src/onsTp1/exercice2Tp1.java
+++ repository/onsTp1/src/onsTp1/exercice2Tp1.java
@@ -3,14 +3,10 @@ package onsTp1;
 import java.util.Scanner;
 
 public class exercice2Tp1 {
-
+	import java.util.Scanner();
 	public static void main(String[] args) {
 
 		Scanner ons=new Scanner(System.in);
-
-		System.out.print("donner une chaine de caractere:");
-
-		String ch=ons.nextLine();
 
 		System.out.print("donner un entier a:");
 
@@ -19,8 +15,6 @@ public class exercice2Tp1 {
 		System.out.print("donner un entier b:");
 
 		int b=ons.nextInt();
-
-		
 
 		System.out.print("donner un entier c:");
 
@@ -32,14 +26,15 @@ public class exercice2Tp1 {
 
 		double m=(a+b+c)/3;
 
-		System.out.println(ch+" "  +"la somme de trois entiers:" +z);
+		System.out.println("la somme de trois entiers:" +z);
 
-		System.out.println(ch+" "  +"la produit de trois entiers:" +p);
+		System.out.println("la produit de trois entiers:" +p);
 
-		System.out.println(ch+" "  +"la moyenne de trois entiers:" +m);				
+		System.out.println("la moyenne de trois entiers:" +m);				
 
 	}
 
 	 
+
 
 }

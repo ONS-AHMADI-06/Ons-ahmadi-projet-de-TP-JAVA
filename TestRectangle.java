102daf80ddc017fcf3010210fc6a984e41ea5a8a
ONS-AHMADI-06 <ahmadions@fstsbz.u-kairouan.tn>
2025-11-10 11:20:50 +0100

    Add files via upload

---
+++ TestRectangle.java
@@ -0,0 +1,11 @@
+package Rectangle;
+
+public class TestRectangle {
+    public static void main(String[] args) {
+        // Création d’un rectangle avec une longueur et une largeur
+        Rectangle r = new Rectangle(22.5, 15.0);
+
+        // Affichage des résultats
+        r.afficher();
+    }
+}
